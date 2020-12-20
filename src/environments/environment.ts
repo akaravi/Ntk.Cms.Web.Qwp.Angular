ea90c58168a1bb7c78343d75f4ac2a12aace384d
Office <karavi@ntk.ir>
2020-12-20 18:13:23 +0330

    api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@ export const environment = {
   cmsServerConfig : {
     configApiRetry: 1,
     configApiServerPath: 'https://apicms.ir/api/v1/',
+    configApiServerPath_: 'https://1b4c3712df1f.ngrok.io/api/v1/',
     configRouteUploadFileContent: 'https://apifile.ir/api/v1/upload/',
   },
   cmsTokenConfig : {
